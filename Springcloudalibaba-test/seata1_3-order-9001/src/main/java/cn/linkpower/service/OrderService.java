c091f723d9d0fbdabbc55d99e8e24f7a927be7c1
JobsZhen <15915466732@163.com>
2021-11-12 18:21:20 +0800

    删除目录

--- Springcloudalibaba-test/seata1_3-order-9001/src/main/java/cn/linkpower/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package cn.linkpower.service;
-
-import cn.linkpower.entity.Order;
-
-public interface OrderService
-{
-    void create(Order order);
-}
--- Springcloudalibaba-test/seata1_3-storage-9003/src/main/java/cn/linkpower/service/StorageService.java
+++
@@ -1,8 +0,0 @@
-package cn.linkpower.service;
-
-public interface StorageService {
-    /**
-     * 扣减库存
-     */
-    void decrease(Long productId, Integer count);
-}
